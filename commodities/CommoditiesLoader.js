771acb4e666f2fdafa91328bb306fbbc5fb14e71
Carlos Castillo <castillo.st@gmail.com>
2018-11-05 19:45:28 +0100

    Refactor to move headers out of commodities config attribute

--- commodities/CommoditiesLoader.js
+++ commodities/CommoditiesLoader.js
@@ -5,16 +5,17 @@ const request = require('request');
 const JSONStream = require('JSONStream');
 
 const camelCasePropertyTransformer = require('./camelCasePropertyTransformer');
+const defaultConfig = require('../config/config');
 
 class CommoditiesLoader {
-  constructor({ url, headers }) {
+  constructor(url = defaultConfig.eddbapi.commodities.url) {
     this.url = url;
-    this.headers = headers;
   }
 
   stream() {
     const unzip_stream = zlib.createGunzip();
-    const { url, headers } = this;
+    const { url } = this;
+    const { headers } = defaultConfig;
     return request({ url, headers })
       .pipe(unzip_stream)
       .pipe(JSONStream.parse('*'))
--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 'use strict';
 
-const { eddbapi } = require('./config/config');
 const CommoditiesLoader = require('./commodities/CommoditiesLoader');
-const loader = new CommoditiesLoader(eddbapi.commodities);
 
-
-const loadStream = loader.stream();
-loadStream.on('data', (data) => console.log(JSON.stringify(data)));
-loadStream.on('error', (err) => console.log('unexpected error', err));
-loadStream.on('end', () => console.log('Load finished'));
-
+module.exports = {
+  CommoditiesLoader
+};
