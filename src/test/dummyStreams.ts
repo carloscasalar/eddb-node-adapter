9c4d0c307fb28a968d19adf8061194ad9d39f07b
Carlos Castillo <castillo.st@gmail.com>
2021-03-04 22:27:22 +0100

    Fix dummy stream typing

--- src/test/dummyStreams.ts
+++ src/test/dummyStreams.ts
@@ -1,13 +1,13 @@
-import {Readable, Stream, Writable} from 'stream';
+import {Readable, Writable} from 'stream';
 
-export const newDummyReadStream = ():Stream => new Readable({
+export const newDummyReadStream = ():Readable => new Readable({
   objectMode: true,
   // eslint-disable-next-line no-empty-function,@typescript-eslint/no-empty-function
   read: () =>{},
 });
 
 export type SaveIncomingChunkFn<DataType> = (data: DataType) => void
-export const newDummyWriteStream = <DataType>(saveIncomingChunkFn: SaveIncomingChunkFn<DataType>):Stream => new Writable({
+export const newDummyWriteStream = <DataType>(saveIncomingChunkFn: SaveIncomingChunkFn<DataType>):Writable => new Writable({
   objectMode: true,
   write(data: DataType) {
     if(saveIncomingChunkFn)
