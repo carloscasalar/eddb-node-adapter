b11220a2e39b742bf90ad5dfcacadb137b101622
Carlos Castillo <castillo.st@gmail.com>
2021-03-07 16:19:05 +0100

    Add station loader an example in TS

---
+++ src/stations/StationsLoader.ts
@@ -0,0 +1,17 @@
+import { Readable } from 'stream';
+import zlib from 'zlib';
+import request from 'request';
+import JSONStream from 'JSONStream';
+import { stationTransformer } from './stationTransformer';
+import defaultConfig from '../config/config.json';
+
+export class StationsLoader {
+  constructor(private url = defaultConfig.eddbapi.stations.url) {}
+
+  stream(): Readable {
+    const unzipStream = zlib.createGunzip();
+    const { url } = this;
+    const { headers } = defaultConfig;
+    return request({ url, headers }).pipe(unzipStream).pipe(JSONStream.parse('*')).pipe(stationTransformer);
+  }
+}
