070010a0fc5b239a35fa975a433ecca836b52b90
Carlos Castillo <castillo.st@gmail.com>
2021-03-07 23:54:40 +0100

    fix ts config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { defaults } = require('jest-config');
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transformIgnorePatterns: [...defaults.transformIgnorePatterns, '<rootDir>/dist/'],
 };
