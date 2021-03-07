a943844cc789849209c52eeef1192b685a807ca7
Carlos Castillo <castillo.st@gmail.com>
2021-03-07 22:55:11 +0100

    Add System Transformer in TS

---
+++ src/systems/systemTransformer.test.ts
@@ -0,0 +1,103 @@
+/* eslint-disable jest/no-done-callback */
+import { newDummyReadStream, newDummyWriteStream } from '../test/dummyStreams';
+
+import { systemTransformer } from './systemTransformer';
+import { System } from './schema';
+
+describe('systemTransformer tests', () => {
+  test('should transform underscore attributes into camelcase ones', (done) => {
+    // eslint-disable-next-line
+    let transformedSystem = {};
+
+    const readStream = newDummyReadStream();
+    readStream.push({
+      id: 17797994,
+      edsm_id: 22972695,
+      name: 'Nyeajaae VU-Y a27-9',
+      x: -3374,
+      y: -42.375,
+      z: 6907.96875,
+      population: 0,
+      is_populated: true,
+      government_id: 208,
+      government: 'Prison',
+      allegiance_id: 4,
+      allegiance: 'Independent',
+      states: ['None'],
+      security_id: 64,
+      security: 'Anarchy',
+      primary_economy_id: 10,
+      primary_economy: 'None',
+      power: 'Yuri Grom',
+      power_state: 'Exploited',
+      power_state_id: 32,
+      needs_permit: false,
+      updated_at: 333,
+      simbad_ref: 'abc',
+      controlling_minor_faction_id: 75876,
+      controlling_minor_faction: 'Independent Detention Foundation',
+      reserve_type_id: 3,
+      reserve_type: 'Common',
+      minor_faction_presences: [
+        {
+          minor_faction_id: 75876,
+          happiness_id: 1,
+          influence: 0,
+          active_states: [{ id: 67, name: 'Expansion' }],
+          pending_states: [{ id: 67, name: 'Expansion' }],
+          recovering_states: [{ id: 67, name: 'Expansion' }],
+        },
+      ],
+    });
+    readStream.push(null);
+
+    const writeStream = newDummyWriteStream<System>((data) => (transformedSystem = data));
+
+    const fullStream = readStream.pipe(systemTransformer).pipe(writeStream);
+
+    fullStream.on('end', () => {
+      expect(transformedSystem).toEqual({
+        id: 17797994,
+        edsmId: 22972695,
+        name: 'Nyeajaae VU-Y a27-9',
+        x: -3374,
+        y: -42.375,
+        z: 6907.96875,
+        population: 0,
+        isPopulated: true,
+        governmentId: 208,
+        government: 'Prison',
+        allegianceId: 4,
+        allegiance: 'Independent',
+        states: ['None'],
+        securityId: 64,
+        security: 'Anarchy',
+        primaryEconomyId: 10,
+        primaryEconomy: 'None',
+        power: 'Yuri Grom',
+        powerState: 'Exploited',
+        powerStateId: 32,
+        needsPermit: false,
+        updatedAt: 333,
+        simbadRef: 'abc',
+        controllingMinorFactionId: 75876,
+        controllingMinorFaction: 'Independent Detention Foundation',
+        reserveTypeId: 3,
+        reserveType: 'Common',
+        minorFactionPresences: [
+          {
+            minorFactionId: 75876,
+            happinessId: 1,
+            influence: 0,
+            activeStates: [{ id: 67, name: 'Expansion' }],
+            pendingStates: [{ id: 67, name: 'Expansion' }],
+            recoveringStates: [{ id: 67, name: 'Expansion' }],
+          },
+        ],
+      });
+      done();
+    });
+
+    fullStream.on('error', (err) => done(err));
+  });
+});
---
+++ src/systems/systemTransformer.ts
@@ -0,0 +1,92 @@
+import { Transform } from 'stream';
+import { RemoteSystem, System } from './schema';
+
+type TransformCallback = (error?: Error | null, system?: System) => void;
+type SystemTransformer = (system: RemoteSystem, chunk: unknown, done: TransformCallback) => void;
+
+const transform: SystemTransformer = (system, _, done) => {
+  const {
+    id,
+    edsm_id: edsmId,
+    name,
+    x,
+    y,
+    z,
+    population,
+    is_populated: isPopulated,
+    government_id: governmentId,
+    government,
+    allegiance_id: allegianceId,
+    allegiance,
+    states,
+    security_id: securityId,
+    security,
+    primary_economy_id: primaryEconomyId,
+    primary_economy: primaryEconomy,
+    power,
+    power_state: powerState,
+    power_state_id: powerStateId,
+    needs_permit: needsPermit,
+    updated_at: updatedAt,
+    simbad_ref: simbadRef,
+    controlling_minor_faction_id: controllingMinorFactionId,
+    controlling_minor_faction: controllingMinorFaction,
+    reserve_type_id: reserveTypeId,
+    reserve_type: reserveType,
+    minor_faction_presences,
+  } = system;
+
+  const minorFactionPresences = minor_faction_presences.map(
+    ({
+      minor_faction_id: minorFactionId,
+      happiness_id: happinessId,
+      influence,
+      active_states: activeStates,
+      pending_states: pendingStates,
+      recovering_states: recoveringStates,
+    }) => ({
+      minorFactionId,
+      happinessId,
+      influence,
+      activeStates,
+      pendingStates,
+      recoveringStates,
+    }),
+  );
+
+  done(null, {
+    id,
+    edsmId,
+    name,
+    x,
+    y,
+    z,
+    population,
+    isPopulated,
+    governmentId,
+    government,
+    allegianceId,
+    allegiance,
+    states,
+    securityId,
+    security,
+    primaryEconomyId,
+    primaryEconomy,
+    power,
+    powerState,
+    powerStateId,
+    needsPermit,
+    updatedAt,
+    simbadRef,
+    controllingMinorFactionId,
+    controllingMinorFaction,
+    reserveTypeId,
+    reserveType,
+    minorFactionPresences,
+  });
+};
+
+export const systemTransformer = new Transform({
+  objectMode: true,
+  transform,
+});
