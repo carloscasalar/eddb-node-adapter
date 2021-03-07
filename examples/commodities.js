dd8dc593ea8d9c4ab61e7ed1e480adc5ba8fe1c1
Carlos Castillo <castillo.st@gmail.com>
2021-03-07 23:06:00 +0100

    🔥Remove all js code

--- examples/commodities.js
+++
@@ -1,9 +0,0 @@
-const { CommoditiesLoader } = require('../');
-
-const commoditiesLoader = new CommoditiesLoader();
-
-const loadStream = commoditiesLoader.stream();
-
-loadStream.on('data', (commodity) => console.dir(commodity, { depth: null, colors: true }));
-loadStream.on('error', (err) => console.log('unexpected error', err));
-loadStream.on('end', () => console.log('Load finished'));
